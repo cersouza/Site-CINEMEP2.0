157a1197dd8d2b0d6cfaee494de84c87d4b3b31b
Caio Eduardo <48000601+cersouza@users.noreply.github.com>
2019-04-12 14:42:20 -0300

    [V002]* Implementando Bootstrap

---
+++ Site-CINEMEP/filmes.php
@@ -0,0 +1,18 @@
+﻿<?php
+/* 	$pag1= "ativado";
+	$pag2 = '#';
+	$pag3 = '#';
+	$pag4 = '#';*/
+	include_once("cabecalho.php");
+?>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12 fotos">
+				<figure>
+					<img class="img-fluid" src="img/filmes/capita-marvel.jpg">
+					<figcaption class="figcaption"> <h3>Capitã Marvel</h3> </figcaption>
+				</figure>
+			</div>
+		</div>
+	</div>
+<?php include_once("rodape.php")?>
