0fd082e58b6072f83cb33ee0be6284e8c233e531
cersouza <souza.caioeduardo@gmail.com>
2019-04-17 14:38:29 -0300

    [V002*] Backup, Implementando Bootstrap - Navbar

--- README.md
+++ README.md
@@ -2,6 +2,9 @@ Projeto Desenvolvimento de Sistemas para Internet:
 
 **Objetivo:** Criar Página de interação com usuários para realizarem comentários sobre os filmes que assistiram.
 
+**Status:**
+- Implmentando Bootstrap em todo Site e Criando Página "Filme".
+
 **Referências:**
 
 - https://www.rottentomatoes.com/m/captain_marvel
--- filmes.php
+++ filmes.php
@@ -1,9 +1,5 @@
 ﻿<?php
-/* 	$pag1= "ativado";
-	$pag2 = '#';
-	$pag3 = '#';
-	$pag4 = '#';*/
-	include_once("cabecalho.php");
+	include_once("cabecalho-novo.php");
 ?>	
 		<div class="row">
 			<div class="col-md-12">
@@ -12,28 +8,26 @@
 					<div class="panel-body">
 
 						<div class="row">
-							<div class="col-md-4">
+							<div class="col-md-4 d-flex justify-content-center">
 								<img class="img-thumbnail img-responsive" src="img/filmes/capita-marvel.jpg">
 							</div>
 						
 							<div class="col-md-8">
 								<table class="table">
 									<tbody>
-										<tr>								
-											<td>
-												<tr><th>Título:</th><td>Capitã Marvel</td></tr>							
-												<tr><th>Data Lançamento:</th><td>7 de março de 2019 (2h 04min)</td></tr>
-												<tr><th>Elenco:</th><td>Brie Larson, Samuel L. Jackson, Jude Law</td></tr>
-												<tr><th>Sinopse:</th>
-													<td><p>
-														Carol Danvers (Brie Larson) é uma ex-agente da Força Aérea norte-americana, que, 
-														sem se lembrar de sua vida na Terra, é recrutada pelos Kree para fazer parte de seu exército de elite. 
-														Inimiga declarada dos Skrull, ela acaba voltando ao seu planeta de origem para impedir uma invasão dos
-														metaformos, e assim vai acabar descobrindo a verdade sobre si, com a ajuda do agente
-														Nick Fury (Samuel L. Jackson) e da gata Goose.
-													</p></td>
-												</tr>
-											</td>
+										<tr>
+											<tr><th>Título:</th><td>Capitã Marvel</td></tr>							
+											<tr><th>Data Lançamento:</th><td>7 de março de 2019 (2h 04min)</td></tr>
+											<tr><th>Elenco:</th><td>Brie Larson, Samuel L. Jackson, Jude Law</td></tr>
+											<tr><th>Sinopse:</th>
+												<td><p>
+													Carol Danvers (Brie Larson) é uma ex-agente da Força Aérea norte-americana, que, 
+													sem se lembrar de sua vida na Terra, é recrutada pelos Kree para fazer parte de seu exército de elite. 
+													Inimiga declarada dos Skrull, ela acaba voltando ao seu planeta de origem para impedir uma invasão dos
+													metaformos, e assim vai acabar descobrindo a verdade sobre si, com a ajuda do agente
+													Nick Fury (Samuel L. Jackson) e da gata Goose.
+												</p></td>
+											</tr>
 										</tr>
 									</tbody>
 								</table>
@@ -43,4 +37,4 @@
 				</div>
 			</div>
 		</div>	
-<?php include_once("rodape.php")?>
+<?php include_once("rodape-novo.php")?>
