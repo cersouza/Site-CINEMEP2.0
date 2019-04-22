f4989cd80f2df301ced12685fdf1ad9764dc00b0
cersouza <souza.caioeduardo@gmail.com>
2019-04-22 14:28:59 -0300

    [V002**] Adicionando .card e área de comentários

--- filmes.php
+++ filmes.php
@@ -3,21 +3,19 @@
 ?>	
 		<div class="row">
 			<div class="col-md-12">
-				<!-- Criando um Card, definido o paddingY (py - top and bottom) e o paddingX (px - left and right);
-					Definir se faz tudo com .card ou com .panel - CERSZ -->
-				<div class="card bg-light py-md-3 px-md-4">
-					<div class="card-header"><h3 class="card-title">Capitã Marvel</h3></div>
+				<!-- Criando um Card, definido o paddingY (py - top and bottom) e o paddingX (px - left and right) - CERSZ -->
+				<div class="card bg-light">
+					<div class="card-header text-primary"><h3 class="card-title">Capitã Marvel</h3></div>
 					<div class="card-body">
 
 						<div class="row">
-							<div class="col-md-4 d-flex justify-content-center">
-								<img class="img-thumbnail" src="img/filmes/capita-marvel.jpg">
+							<div class="col-md-4 d-flex justify-content-center align-items-center">
+								<img class="rounded" src="img/filmes/capa-capita-marvel.jpg" width="auto" height="400">
 							</div>
 						
 							<div class="col-md-8">
 								<table class="table">
 									<tbody>
-										<tr>
 											<tr><th>Título:</th><td>Capitã Marvel</td></tr>							
 											<tr><th>Data Lançamento:</th><td>7 de março de 2019 (2h 04min)</td></tr>
 											<tr><th>Elenco:</th><td>Brie Larson, Samuel L. Jackson, Jude Law</td></tr>
@@ -30,13 +28,71 @@
 													Nick Fury (Samuel L. Jackson) e da gata Goose.
 												</p></td>
 											</tr>
-										</tr>
+											<tr><th>Nota Usuários:</th><td><h1 class="text-warning d-inline">***</h1></h1 class="text-muted d-inline"> - Regular<h1></td></tr>
 									</tbody>
 								</table>
 							</div>
 						</div>
 					</div>
 				</div>
+				
+				<div class="card bg-light">
+					<div class="card-header text-primary"><h2 class="card-title">Comentários</h2></div>
+					<div class="card-body">			
+
+						<!-- Criando um Bloco para comentários (.cards) -->
+						<div class="card-columns bg-light">
+							
+							<?php for($x = 0; $x < 4; $x++){?>
+
+							<div class="card bg-class">
+								<div class="card-body">	
+								<h2 class="card-title text-warning d-inline">***</h2>
+								<h3 class="card-subtitle text-muted d-inline"> - Muito Bom!</h3>				
+									<blockquote class="blockquote mb-0">
+										<p>Eu gostei muito do filme, recomendo!!</p>
+										<footer class="blockquote-footer">
+											J. Claudiano<cite title="Título da fonte">, 12 Abr</cite>									
+										</footer>
+									</blockquote>
+									<hr>
+									<a href="#" class="card-link">Curti!</a>
+									<a href="#" class="card-link">Não Gostei!</a>
+									<a href="#" class="card-link">Quero Comentar</a>
+								</div>
+							</div>
+
+							<?php } ?>						
+							
+						</div>
+						<!-- fim .card-columns -->
+
+						<div class="card bg-light">
+								<div class="card-header text-center">
+									<h3 class="card-title">Adicionar Comentário</h3>
+									<button class="btn btn-primary" data-toggle="collapse" data-target="#frm-comentario">Comentar</button>
+								</div>
+								<div class="card-body collapse" id="frm-comentario">									
+									<form class="form-group" method="POST" action="#">
+										<label for="nota">Nota:</label>
+										<select class="form-control w-50" id="nota">
+											<option>1 - Péssimo</option>
+											<option>2 - Ruim</option>
+											<option>3 - Mais ou Menos</option>
+											<option>4 - Bom</option>
+											<option>5 - Excelente</option>
+										</select>
+
+										<label for="comment">Comentário:</label>
+										<textarea class="form-control" rows="5" id="comment"  name="comentario" placeholder="Digite seu comentário aqui..."></textarea><br>
+										<input class="btn btn-primary" type="submit" value="Publicar">
+									</form>
+								</div>
+							</div>
+
+					</div>
+				</div>
+
 			</div>
 		</div>	
 <?php include_once("rodape-novo.php")?>
